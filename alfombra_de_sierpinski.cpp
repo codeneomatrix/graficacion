98ef93571ae17520127e71b816bfa3effa893616
neomatrix <josuecevedo@gmail.com>
2015-08-27 20:32:35 -0500

    commit inicial

---
+++ alfombra_de_sierpinski.cpp
@@ -0,0 +1,19 @@
+/*
+  program:1
+  Name: alfombra de sierpinski.cpp
+  Author: josue acevedo maldonado, maldad, Luis E. Ramos Cruz
+  Date: 27/08/15 
+  Description:
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+ main()
+{
+ 
+   printf("Alfombra de sierpinski \n");
+    system("pause");
+    return 0;
+    }
+

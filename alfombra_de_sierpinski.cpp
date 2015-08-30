3a174e49056f8ee860ae49496bf3460d6697883f
neomatrix <josuecevedo@gmail.com>
2015-08-29 19:03:44 -0500

    implementando las funciones

--- alfombra_de_sierpinski.cpp
+++ alfombra_de_sierpinski.cpp
@@ -9,18 +9,104 @@
 #include <stdlib.h>
 #include <math.h>
 
-void pintar(){
+void pintar(int pi,int pf,char le[]){
+
+	for (int i=0; i<pf; i++){
+    	for (int j = 0; j < pf; j++)
+    	{
+    		printf("%s", le);	
+    	}
+    	printf("\n");
+   	//espacio(e);
+    }
 
 }
 
-void dividir(){
+void dividir(int pi, int pf){
+	int t,pc1=0,pc2=0;
+	t= (pf-pi)/3;
+	pc1= pi+t;
+	pc2= pc1+t;
+	printf("pc1: %d\n",pc1 );
+	printf("pc2: %d\n",pc2 );
 	
 }
 
- main()
+void espacio(int p){
+	for (int j = 0; j < p; j++)
+    	{
+    		printf("%s", " ");	
+    	}
+}
+main()
 {
- 
-   printf("Alfombra de Sierpinski \n");
+    int e=20,r=40,n,mx1,mx2,my;
+    //r=80
+
+    printf("Alfombra de Sierpinski \n");
+    
+    printf("ingrese el nivel :\n");
+    scanf("%d",&n);
+
+    char x[]=".";
+    pintar(0,27,x);
+
+	//espacio(e);
+
+    /*for (int i=0; i<mx1; i++){
+    	for (int j = 0; j < mx1; j++)
+    	{
+    		printf("%s", "*");	
+    	}
+
+    	for (int j = mx1; j < mx2; j++)
+    	{
+    		printf("%s", "%");	
+    	}
+    	for (int j = mx2; j < r; j++)
+    	{
+    		printf("%s", "=");	
+    	}
+    	printf("\n");
+   	espacio(e);
+    }
+     for (int i=mx1; i<mx2; i++){
+    	for (int j = 0; j < mx1; j++)
+    	{
+    		printf("%s", "/");	
+    	}
+
+    	for (int j = mx1; j < mx2; j++)
+    	{
+    		printf("%s", "#");	
+    	}
+    	for (int j = mx2; j < r; j++)
+    	{
+    		printf("%s", "!");	
+    	}
+    	printf("\n");
+   	espacio(e);
+    }
+    for (int i=mx2; i<r; i++){
+    	for (int j = 0; j < mx1; j++)
+    	{
+    		printf("%s", "+");	
+    	}
+
+    	for (int j = mx1; j < mx2; j++)
+    	{
+    		printf("%s", "~");	
+    	}
+    	for (int j = mx2; j < r; j++)
+    	{
+    		printf("%s", "¨");	
+    	}
+    	printf("\n");
+   	espacio(e);
+    }*/
+
+    dividir(0,27);
+
     system("pause");
     return 0;
     }

e8a478ca8f9cf39d47420d3ed1a7b29281f7d5fd
codeneomatrix <josuecevedo@gmail.com>
2015-12-06 13:15:37 -0600

    dado con texturas

--- cubo.cpp
+++ cubo.cpp
@@ -12,7 +12,7 @@ GLfloat xi = 0.0;
 GLfloat yi = 0.0;
 //GLint numLados = 3;
 GLint escala = 1;
-
+const float BOX_SIZE = 4.0f;
 GLdouble anguloY = 0;
 GLdouble anguloX = 0;
 GLdouble anguloZ = 0;
@@ -24,15 +24,44 @@ GLuint loadTexture(Image* image) {
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image->width, image->height, 0, GL_RGB, GL_UNSIGNED_BYTE, image->pixels);
 	return idtextura;
 }
-GLuint _madera_pared;
+GLuint _uno;
+GLuint _dos;
+GLuint _tres;
+GLuint _cuatro;
+GLuint _cinco;
+GLuint _seis;
 void initRendering() {
-	Image* madera_pared = loadBMP("dado.jpg");
-	_madera_pared = loadTexture(madera_pared);
-	delete madera_pared;
+	
+	Image* uno = loadBMP("1.bmp");
+	_uno = loadTexture(uno);
+	delete uno;
+	Image* dos = loadBMP("2.bmp");
+	_dos = loadTexture(dos);
+	delete dos;
+	Image* tres = loadBMP("3.bmp");
+	_tres = loadTexture(tres);
+	delete tres;
+	Image* cuatro = loadBMP("4.bmp");
+	_cuatro = loadTexture(cuatro);
+	delete cuatro;
+	Image* cinco = loadBMP("5.bmp");
+	_cinco = loadTexture(cinco);
+	delete cinco;
+	Image* seis = loadBMP("6.bmp");
+	_seis = loadTexture(seis);
+	delete seis;
 }
 
-void init(void){
-	glClearColor(0.0, 0.0, 0.0, 0.0);
+void init(void){							
+ glEnable(GL_TEXTURE_2D);							
+ glShadeModel(GL_SMOOTH);							
+ glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				
+ glClearDepth(1.0f);									
+ glEnable(GL_DEPTH_TEST);							
+ glDepthFunc(GL_LEQUAL);								
+ glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	
+
+ glEnable(GL_LIGHT1);		
 }
 
 
@@ -102,16 +131,11 @@ void ArrowKey(int key, int x, int y){
 
 
 void display(void){
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
+    
 	
 	glLoadIdentity();
-
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, _madera_pared);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
 	gluLookAt(0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	glPushMatrix();
 	glTranslatef(xi, yi, -3);
@@ -122,67 +146,117 @@ void display(void){
 	glRotated(anguloX, 1.0, 0.0, 0.0);
 	glRotated(anguloY,0.0,1.0,0.0);
 	glRotated(anguloZ, 0.0, 0.0, 1.0);
-	
-	//LADO TRASERO verdefuerte
+
+	//Top face
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _uno);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glBegin(GL_POLYGON);
-
-	//glColor3f(0.0, 0.4, 0.3);    
-	glVertex3f(0.5, -0.5, -0.5); 
-   
-	glVertex3f(0.5, 0.5, -0.5);      
-     
-	glVertex3f(-0.5, 0.5, -0.5);     
-    
-	glVertex3f(-0.5, -0.5, -0.5);     
-
+	glColor3f(1.0f, 1.0f, 1.0f); 
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-BOX_SIZE / 2 , BOX_SIZE / 2, -BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(-BOX_SIZE / 2 , BOX_SIZE / 2, BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 1.0f);	
+	glVertex3f(BOX_SIZE / 2 , BOX_SIZE / 2, BOX_SIZE / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(BOX_SIZE / 2 , BOX_SIZE / 2, -BOX_SIZE / 2);
 	glEnd();
-
-	// LADO FRONTAL: lado amarillo
+	glDisable(GL_TEXTURE_2D);
+ 
+	//Bottom face
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _dos);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glBegin(GL_POLYGON);
-	//glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(0.5, -0.5, 0.5);
-	glVertex3f(0.5, 0.5, 0.5);
-	glVertex3f(-0.5, 0.5, 0.5);
-	glVertex3f(-0.5, -0.5, 0.5);
+	glColor3f(1.0f, 1.0f, 1.0f); 
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-BOX_SIZE / 2  , -BOX_SIZE / 2, -BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 0.0f);
+        glVertex3f(BOX_SIZE / 2  , -BOX_SIZE / 2, -BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 1.0f);
+        glVertex3f(BOX_SIZE / 2  , -BOX_SIZE / 2, BOX_SIZE / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(-BOX_SIZE / 2  , -BOX_SIZE / 2, BOX_SIZE / 2);
 	glEnd();
-
-	// LADO Izquierdo: lado morado
+	glDisable(GL_TEXTURE_2D);
+ 
+ 
+	//Left face 
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _tres);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glBegin(GL_POLYGON);
-	//glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.5, -0.5, -0.5);
-	glVertex3f(0.5, 0.5, -0.5);
-	glVertex3f(0.5, 0.5, 0.5);
-	glVertex3f(0.5, -0.5, 0.5);
+	glColor3f(1.0f, 1.0f, 1.0f); 
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-BOX_SIZE / 2  , -BOX_SIZE / 2, -BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(-BOX_SIZE / 2  , -BOX_SIZE / 2, BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(-BOX_SIZE / 2  , BOX_SIZE / 2, BOX_SIZE / 2);
+	glTexCoord2f(0.0f, 1.0f);
+        glVertex3f(-BOX_SIZE / 2  , BOX_SIZE / 2, -BOX_SIZE / 2);
 	glEnd();
-
-	// LADO derecho: lado verde
+	glDisable(GL_TEXTURE_2D);
+ 
+	//Right face
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _cuatro);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glBegin(GL_POLYGON);
-	//glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(-0.5, -0.5, 0.5);
-	glVertex3f(-0.5, 0.5, 0.5);
-	glVertex3f(-0.5, 0.5, -0.5);
-	glVertex3f(-0.5, -0.5, -0.5);
+	glColor3f(1.0f, 1.0f, 1.0f); 
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(BOX_SIZE / 2 , -BOX_SIZE / 2, -BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(BOX_SIZE / 2 , BOX_SIZE / 2, -BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(BOX_SIZE / 2 , BOX_SIZE / 2, BOX_SIZE / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(BOX_SIZE / 2 , -BOX_SIZE / 2, BOX_SIZE / 2);
 	glEnd();
-
-	// LADO SUPERIOR: lado azul
+	glDisable(GL_TEXTURE_2D);
+ 
+	//Front face : Polígono Texturizado
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _cinco);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glBegin(GL_POLYGON);	
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-BOX_SIZE / 2 , -BOX_SIZE / 2, BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(BOX_SIZE / 2 , -BOX_SIZE / 2, BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(BOX_SIZE / 2 , BOX_SIZE / 2, BOX_SIZE / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(-BOX_SIZE / 2 , BOX_SIZE / 2, BOX_SIZE / 2);
+	glEnd();	
+	glDisable(GL_TEXTURE_2D);
+ 
+	//Back face : Polígono Texturizado
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _seis);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glColor3f(1.0f, 1.0f, 1.0f);
 	glBegin(GL_POLYGON);
-	//glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(0.5, 0.5, 0.5);
-	glVertex3f(0.5, 0.5, -0.5);
-	glVertex3f(-0.5, 0.5, -0.5);
-	glVertex3f(-0.5, 0.5, 0.5);
+	glNormal3f(0.0, 0.0f, -1.0f);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-BOX_SIZE / 2, -BOX_SIZE / 2, -BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(-BOX_SIZE / 2, BOX_SIZE / 2, -BOX_SIZE / 2);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(BOX_SIZE / 2, BOX_SIZE / 2, -BOX_SIZE / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(BOX_SIZE / 2, -BOX_SIZE / 2, -BOX_SIZE / 2);	
 	glEnd();
-
-	// LADO INFERIOR: lado rojo
-	glBegin(GL_POLYGON);
-	//glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(0.5, -0.5, -0.5);
-	glVertex3f(0.5, -0.5, 0.5);
-	glVertex3f(-0.5, -0.5, 0.5);
-	glVertex3f(-0.5, -0.5, -0.5);
-	glEnd();
-	glPopMatrix(); // Cierra la matriz
-	glFlush();
+	glDisable(GL_TEXTURE_2D);
+ 
 	glutSwapBuffers();
 }
 void reshape(int w, int h){
@@ -206,7 +280,7 @@ void keyboard(unsigned char key, int x, int y){
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(400, 300);
 
 	glutInitWindowPosition(100, 100);

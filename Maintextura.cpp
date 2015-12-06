b68ac8b33645aaed316cc1efcb372db0a871a07d
neomatrix <josuecevedo@gmail.com>
2015-12-05 21:45:20 -0600

    cargando texturas

---
+++ Maintextura.cpp
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+#include <GL\glut.h>
+#include "ImageLoader.h"
+GLuint loadTexture(Image* image) {
+	GLuint idtextura;
+	glGenTextures(1, &idtextura);
+	glBindTexture(GL_TEXTURE_2D, idtextura);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image->width, image->height, 0, GL_RGB, GL_UNSIGNED_BYTE, image->pixels);
+	return idtextura;
+}
+GLuint _madera_pared;
+void initRendering() {
+	Image* madera_pared = loadBMP("madera_pared.bmp");
+	_madera_pared = loadTexture(madera_pared);
+	delete madera_pared;
+}
+void display(void) {
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _madera_pared);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex2f(-0.5, -0.5);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex2f(-0.5, 0.5);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex2f(0.5, 0.5);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex2f(0.5, -0.5);
+	glEnd();
+
+	glutSwapBuffers();
+}
+int main(int argc, char ** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutCreateWindow("Hello World");
+	initRendering();
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
